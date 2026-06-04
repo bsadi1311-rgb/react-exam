ef063b7fe6942bf8b002f80890246ee330c54322
Sadi <bsadi.1311bsadi1311@gmail.com>
2026-06-04 14:21:31 +0300

    first commit

--- src/router/router.js
+++ src/router/router.js
@@ -7,7 +7,7 @@ export const Home = lazy(() => import("../components/Home"));
 export const Contact = lazy(() => import("../components/page/Contac"));
 
 // Наши проекты
-export const Nasha = lazy(() => import("../components/page1/nasha"));
+export const Nasha = lazy(() => import("../components/page1/Nasha"));
 export const Comanda = lazy(() => import("../components/page2/Comanda"));
 export const Dom = lazy(() => import("../components/page3/Dom"));
 export const OdnaDom = lazy(() => import("../components/page4/OdnaDom"));
